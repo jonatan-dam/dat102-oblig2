98e8172631a377c389fd8681ad28c4030bea7b5f
h678278 <142307949+h678278@users.noreply.github.com>
2024-02-12 08:51:57 +0100

    Småendringer

--- src/no/hvl/dat102/sortering/Main.java
+++ src/no/hvl/dat102/sortering/Main.java
@@ -12,18 +12,21 @@ public class Main {
 		
 		for(int i = 0; i < n; i++) {
 			Integer[] tabell = createRandom();
+			
+			
+			
 			long startMillis = System.currentTimeMillis();
 			
-			SortArray.insertionSort(tabell, 0, 99999);
+			SortArray.insertionSort(tabell);
 			
-			
+		
 			long kjoreTid = System.currentTimeMillis() - startMillis;
 			sum = sum + kjoreTid;
 			//System.out.println("Kjøretid er: " + kjoreTid + "ms");
 			
 		}
 		
-		double snittTid = (double) sum/5;
+		double snittTid = (double) sum/n;
 		System.out.println("Gjennomsnittlig kjøretid over " + n + " kjøringer er: " + snittTid + "ms");
 		
 		
--- src/no/hvl/dat102/sortering/SortArray.java
+++ src/no/hvl/dat102/sortering/SortArray.java
@@ -4,7 +4,14 @@ package no.hvl.dat102.sortering;
 public class SortArray {
 	
 	
+	public static <T extends Comparable<? super T>> void insertionSort(T[] a) {
+		//minsteForst(a);
+		insertionSort(a, 0, a.length - 1);
+	}
+	
+	
 	public static <T extends Comparable<? super T>> void insertionSort(T[] a, int forste, int siste) {
+		
 		for(int i = forste + 1; i <= siste; i++) {
 			
 			T temp = a[i];
@@ -20,7 +27,21 @@ public class SortArray {
 	}
 	
 	
-	
+	public static <T extends Comparable<? super T>> void minsteForst(T[] a) {
+		
+		int minsteIndex = 0;
+		for(int i = 0; i < a.length; i++) {
+			if(a[i].compareTo(a[minsteIndex]) < 0) {
+				minsteIndex = i;
+			}
+		}
+		
+		if (minsteIndex != 0) {
+			T temp = a[0];
+			a[0] = a[minsteIndex];
+			a[minsteIndex] = temp;
+		}
+	}
 	
 	
 	
